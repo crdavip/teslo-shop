cdaff478238043e2396db5c3863332223cdb6c18
crdavip <crdavip@gmail.com>
2025-07-22 18:54:27 -0500

    Seed categories created

--- src/seed/seed-db.ts
+++ src/seed/seed-db.ts
@@ -1,11 +1,25 @@
 import { initialData } from "./seed";
+import { prisma } from "../lib/prisma";
 
 async function main() {
-  console.log(initialData);
+  await prisma.productImage.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
 
-  console.log("Seed ejecutado");
+  const { categories } = initialData;
+  const categoriesData = categories.map((category) => ({
+    name: category,
+  }));
+
+  await prisma.category.createMany({
+    data: categoriesData,
+  });
+
+  console.log("Seed successfully executed");
 }
 
 (() => {
+  if (process.env.NODE_ENV === "production") return;
+
   main();
 })();
