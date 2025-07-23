cd7c4aed15cd18f84be3fe61310b9b6fa531ab02
crdavip <crdavip@gmail.com>
2025-07-22 19:05:31 -0500

    Seed products and products images created

--- src/seed/seed-db.ts
+++ src/seed/seed-db.ts
@@ -6,13 +6,38 @@ async function main() {
   await prisma.product.deleteMany();
   await prisma.category.deleteMany();
 
-  const { categories } = initialData;
+  const { categories, products } = initialData;
   const categoriesData = categories.map((category) => ({
     name: category,
   }));
 
   await prisma.category.createMany({
     data: categoriesData,
+  });
+
+  const categoriesDB = await prisma.category.findMany();
+  const categoriesMap = categoriesDB.reduce((map, category) => {
+    map[category.name.toLowerCase()] = category.id;
+    return map;
+  }, {} as Record<string, string>);
+
+  products.forEach(async (product) => {
+    const { type, images, ...rest } = product;
+    const dbProduct = await prisma.product.create({
+      data: {
+        ...rest,
+        categoryId: categoriesMap[type],
+      },
+    });
+
+    const imagesData = images.map((image) => ({
+      url: image,
+      productId: dbProduct.id,
+    }));
+
+    await prisma.productImage.createMany({
+      data: imagesData,
+    });
   });
 
   console.log("Seed successfully executed");
