531b91a5d0a6c035f295788b83243245f17f4120
crdavip <crdavip@gmail.com>
2025-07-28 16:03:49 -0500

    Seed archive updated

--- src/seed/seed-db.ts
+++ src/seed/seed-db.ts
@@ -3,9 +3,14 @@ import { initialData } from "./seed";
 import { countries } from "./seed-countries";
 
 async function main() {
+  await prisma.orderAddress.deleteMany();
+  await prisma.orderItem.deleteMany();
+  await prisma.order.deleteMany();
+
   await prisma.userAddress.deleteMany();
   await prisma.user.deleteMany();
   await prisma.country.deleteMany();
+
   await prisma.productImage.deleteMany();
   await prisma.product.deleteMany();
   await prisma.category.deleteMany();
