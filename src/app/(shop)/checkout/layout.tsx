4a549e7bcdcd00c286c617edcbb7b43cc1a7fa7a
crdavip <crdavip@gmail.com>
2025-07-29 20:15:39 -0500

    Admin route protected is check

---
+++ src/app/(shop)/checkout/layout.tsx
@@ -0,0 +1,12 @@
+import { redirect } from "next/navigation";
+import { auth } from "@/auth.config";
+
+export default async function CheckoutLayout({ children }: { children: React.ReactNode }) {
+  const session = await auth();
+
+  if (!session?.user) {
+    redirect("/auth/login");
+  }
+
+  return <>{children}</>;
+}
